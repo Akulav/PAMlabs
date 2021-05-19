bdb672d0ca566e62231f170274af31762c999e79
Akulav <43496076+Akulav@users.noreply.github.com>
2021-05-19 21:58:18 +0300

    Added Dependency. Created Test Object

---
+++ app/src/main/java/com/example/lab2test/Login/AuthUtil.kt
@@ -0,0 +1,12 @@
+package com.example.lab2test.Login
+
+object AuthUtil {
+    val users = listOf("catalin", "petru", "ion)
+
+    fun signUp(
+        userName: String,
+        password: String
+    ): Boolean{
+        return true
+    }
+}
